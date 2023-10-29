1ee0c226a2beb9a9de7430ca28b89d1e6e490387
Den104Ten <dan_eltsov@mail.ru>
2023-10-29 19:01:50 +0300

    com for host

--- Calculator_remonta/settings.py
+++ Calculator_remonta/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+uenwx^vr#6x6n#b8i#6b^337zpfdw&8=ur$cb-@_ia@%m076s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
